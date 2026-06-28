ff7bcc1311387cac9144ba929f6404703823c272
Vignesh1116 <vigneshbalan1116@gmail.com>
2026-06-28 10:15:05 +0530

    Day 2

---
+++ Day 2/ml_project.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.feature_selection import SelectKBest, mutual_info_regression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import os
+
+# Category_encoders is needed for Target Encoding text
+
+try:
+    from category_encoders import TargetEncoder
+except ImportError:
+    TargetEncoder = None
+    print("Warning: category_encoders not installed. Target Encoding will be unavailable.")
+
+def main():
+
+    print("Loading Datasets")   
+    file_path='train.csv'
+
+    if not os.path.exists(file_path):
+        print(f"Error cannot find '{file_path}'")
+        return 
+
+    df=pd.read_csv(file_path)    
+    print(f"Dataset Loaded Successfully.Rows:{df.shape[0]},Features:{df.shape[1]}\n")
+
+    # HANDLING MISSING DATA
+    # Artificially create missing data for the lesson
+    df.loc[0:25, 'H'] = np.nan 
+    
+    imputer = SimpleImputer(strategy='median')
+    
+    df['H'] = imputer.fit_transform(df[['H']])
+    print(f"Imputation complete. 'Hits' (H) now has {df['H'].isnull().sum()} null values.\n")
+     
+
+    #  SKEWED DISTRIBUTION
+    df['LogRuns'] = np.log1p(df['R'])
+    print(f"Log Transformation applied. New skewness: {df['LogRuns'].skew():.2f} (closer to 0 is perfectly balanced).\n")
+    
