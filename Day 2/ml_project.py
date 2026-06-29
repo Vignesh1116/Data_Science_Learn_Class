c30256a6637f2da0355dc061b90ce612fb0b11b2
Vignesh1116 <vigneshbalan1116@gmail.com>
2026-06-29 14:13:17 +0530

    ML project using linear regression

--- Day 2/ml_project.py
+++ Day 2/ml_project.py
@@ -15,7 +15,7 @@ except ImportError:
     print("Warning: category_encoders not installed. Target Encoding will be unavailable.")
 
 def main():
-
+ 
     print("Loading Datasets")   
     file_path='train.csv'
 
@@ -39,4 +39,70 @@ def main():
     #  SKEWED DISTRIBUTION
     df['LogRuns'] = np.log1p(df['R'])
     print(f"Log Transformation applied. New skewness: {df['LogRuns'].skew():.2f} (closer to 0 is perfectly balanced).\n")
+
+    # High Cardinality
+
+    # Creating High Cardinality Manually
+
+    df['Team_ID'] = ['Team_'+str(np.random.randint(1,150)) for _ in range(len(df))] 
     
+    if TargetEncoder is not None:
+        print("Applying Target Encoder")
+        
+        encoder = TargetEncoder()
+
+        df['Team_ID_Encoded'] = encoder.fit_transform(df['Team_ID'],df['W'])  
+
+    else:
+        print("Category Encoders not installed")    
+
+    # Feature Selection
+
+    features_to_test = ['R','HR','SO','SB']
+
+    X_features = df[features_to_test].fillna(0)
+    y_target = df['W']
+
+    selector = SelectKBest(score_func=mutual_info_regression,k=2)
+
+    winning_features = selector.get_support()
+    best_features = X_features.columns[winning_features].tolist()
+
+    print(best_features)
+    
+    # Splitting Data 
+    
+    X = df[best_features]
+    Y = df["W"]
+
+    X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
+
+    print(f"Training Data Size: {X_train.shape}")
+    print(f"Testing Data Size: {X_test.shape}\n")
+
+    # Training Model
+
+    model=LinearRegression()
+    model.fit(X_train,Y_train)
+    
+    predictions = model.predict(X_test)
+    print(predictions)
+
+    # comparing model predictions to the actual real answer
+
+    actual_wins = Y_test.head(3).values
+    predicted_wins=predictions[:3]
+
+    for i in range(3):
+        predicted = round(predicted_wins[i])
+        actual = actual_wins[i]
+        difference=abs(actual-predicted)
+
+        print(f"Model Gussed:{predicted}")
+        print(f"Real Answer:{actual}")
+        print(f"Differences:{difference}")
+
+if __name__ == '__main__':
+    main()    
+
+
